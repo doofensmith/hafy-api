b67fdb9c9b42f836072efd20de79bd8b20a11261
doofensmith <wahyuwijanarko3998@gmail.com>
2022-08-04 11:26:27 +0700

    Base Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hafy-api
+# HaFy API
+#### By : Wahyu Wijanarko / 19312011
 API for Hafy App
---
+++ src/main/java/com/softlaboratory/hafyapi/HafyApiApplication.java
@@ -0,0 +1,13 @@
+package com.softlaboratory.hafyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafyApiApplication.class, args);
+	}
+
+}
