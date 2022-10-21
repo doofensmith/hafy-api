aabe42e542dc8ace90f084c320e319c8cb931cb6
doofensmith <wahyuwijanarko3998@gmail.com>
2022-10-21 14:59:46 +0700

    Roles service

--- src/main/java/com/softlaboratory/hafyapi/controller/RolesController.java
+++ src/main/java/com/softlaboratory/hafyapi/controller/RolesController.java
@@ -1,13 +1,13 @@
 package com.softlaboratory.hafyapi.controller;
 
+import com.softlaboratory.hafyapi.domain.dto.RolesDto;
 import com.softlaboratory.hafyapi.service.RolesService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("/roles")
 public class RolesController {
@@ -16,10 +16,67 @@ public class RolesController {
     private RolesService service;
 
     @GetMapping(value = "")
-    public ResponseEntity<Object> findByRole(@RequestBody String role) {
+    public ResponseEntity<Object> getAll() {
+        try {
+            return service.getAll();
+        }catch (Exception e) {
+            log.info("Error get all : {}", e.getMessage());
+            log.debug("Debug error : ",e);
+            throw e;
+        }
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id) {
+        try {
+            return service.getById(id);
+        }catch (Exception e) {
+            log.info("Error get by id : {}", e.getMessage());
+            log.debug("Debug error : ",e);
+            throw e;
+        }
+    }
+
+    @GetMapping(value = "/q")
+    public ResponseEntity<Object> getByRole(@RequestParam(value = "role") String role) {
         try {
             return service.getByRole(role);
         }catch (Exception e) {
+            log.info("Error get by role : {}", e.getMessage());
+            log.debug("Debug error : ",e);
+            throw e;
+        }
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity<Object> createRole(@RequestBody RolesDto req) {
+        try {
+            return service.create(req);
+        }catch (Exception e) {
+            log.info("Error creating new role : {}", e.getMessage());
+            log.debug("Debug error : ",e);
+            throw e;
+        }
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Object> updateRole(@PathVariable Long id, @RequestBody RolesDto req) {
+        try {
+            return service.updateById(id, req);
+        }catch (Exception e) {
+            log.info("Error updating role : {}", e.getMessage());
+            log.debug("Debug error : ",e);
+            throw e;
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> updateRole(@PathVariable Long id) {
+        try {
+            return service.deleteById(id);
+        }catch (Exception e) {
+            log.info("Error deleting role : {}", e.getMessage());
+            log.debug("Debug error : ",e);
             throw e;
         }
     }
